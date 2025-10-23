196a6e57d97bd136a655ed14e1f17104a7aca59e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-23 18:29:30 -0300

    refactor: cambiar nombre de constante

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,3 +1,3 @@
 export const PROJECT_NAME = 'switch-2-simulator'
-export const PROJECT_PUBLIC_NAME = 'Simulador de Nintendo Switch 2'
+export const PROJECT_USER_FRIENDLY_NAME = 'Simulador de Nintendo Switch 2'
 export const BASE_TITLE = 'Nintendo Switch 2'
