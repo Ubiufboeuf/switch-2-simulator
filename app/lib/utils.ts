e6826ea9129b7b2a4be02b1bfcf3e964e9689117
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-23 18:34:30 -0300

    feat: terminar de preparar las cosas para comenzar con el proyecto

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,5 +1,7 @@
 import { BASE_TITLE } from './constants'
 
-export function title (title: string = '') {
+export function handleTitle (title: string = '') {
   return title ? `${title} - ${BASE_TITLE}` : BASE_TITLE
 }
+
+export const title = handleTitle()
--- app/routes/startup.tsx
+++ app/routes/startup.tsx
@@ -1,9 +1,11 @@
+import { PROJECT_DESCRIPTION } from '~/lib/constants'
 import type { Route } from './+types/startup'
+import { title } from '~/lib/utils'
 
 export function meta ({}: Route.MetaArgs) {
   return [
-    { title: '' },
-    { name: 'description', content: 'Welcome to React Router!' }
+    { title },
+    { name: 'description', content: PROJECT_DESCRIPTION }
   ]
 }
 
