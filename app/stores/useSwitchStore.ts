fb94f96c2835c2b75a4c89118ccba9f6e456653b
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-23 21:04:07 -0300

    feat: crear store para la consola

---
+++ app/stores/useSwitchStore.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand'
+import type { ConsoleState } from '~/env'
+
+type SwitchStore = {
+  isConsoleOn: boolean
+  consoleState: ConsoleState
+  setIsConsoleOn: (newState: boolean) => void
+  setConsoleState: (newState: ConsoleState) => void
+}
+
+export const useSwitchStore = create<SwitchStore>((set) => ({
+  isConsoleOn: false,
+  consoleState: 'off',
+  setIsConsoleOn: (newState) => set({ isConsoleOn: newState }),
+  setConsoleState: (newState) => set({ consoleState: newState })
+}))
