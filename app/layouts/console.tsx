21e8a616ce67514bb1f82183bf95b59ef5f40616
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-24 11:44:27 -0300

    fix: cambiar anchor por link de react router

--- app/layouts/console.tsx
+++ app/layouts/console.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router'
+import { Link, Outlet } from 'react-router'
 
 export default function ConsoleLayout () {
   return (
@@ -6,7 +6,7 @@ export default function ConsoleLayout () {
       <div className='w-full'>
         <img src='/console.svg' className='w-full' />
       </div>
-      <a href='/' className='absolute left-[14.6%] top-[68.1%] w-[5.3%] h-[5.8%] rounded-full transition-colors hover:bg-neutral-300/10 text-blue-400 hover:text-blue-500'></a>
+      <Link to='/map-creator' className='absolute left-[14.6%] top-[68.1%] w-[5.3%] h-[5.8%] rounded-full transition-colors hover:bg-neutral-300/10 text-blue-400 hover:text-blue-500'></Link>
       <Outlet />
     </>
   )
