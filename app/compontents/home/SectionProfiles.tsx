437863520d63fb35b9a26fe32cc01011e73c9a72
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-24 19:16:25 -0300

    feat: :sparkles: implementar sección visual de perfiles

--- app/compontents/home/SectionProfiles.tsx
+++ app/compontents/home/SectionProfiles.tsx
@@ -1,5 +1,31 @@
+import { Avatar } from '../Icons/Avatar'
+import { Icon } from '../Icons/Icon'
+
+const profiles = [
+  {
+    id: 'profile-1',
+    name: 'mario2'
+  },
+  {
+    id: 'profile-2',
+    name: 'luigi2'
+  },
+  {
+    id: 'profile-3',
+    name: 'donkeyKong2'
+  }
+]
+
 export function SectionProfiles () {
   return (
-    ''
+    <section className='absolute left-[27.5px] top-[47px] h-[35.75px] w-[120px] rounded-full flex gap-[6.5px]'>
+      { profiles.map(({ id, name }) => (
+        <article key={`section-profiles-${id}`} className='h-full'>
+          <Icon className='bg-black/50 h-full w-[35.75px] rounded-full overflow-hidden'>
+            <Avatar name={name} className='text-[4px]' />
+          </Icon>
+        </article>
+      )) }
+    </section>
   )
 }
