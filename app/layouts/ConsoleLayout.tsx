bcf17f3427a5c202020e660f32f0594ab3386729
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-24 19:20:02 -0300

    chore: cambiar enlace del botón steam

--- app/layouts/ConsoleLayout.tsx
+++ app/layouts/ConsoleLayout.tsx
@@ -6,7 +6,7 @@ export default function ConsoleLayout () {
       <div className='absolute w-341'>
         <img src='/console.svg' className='w-full' />
       </div>
-      <Link to='/map-creator' className='absolute left-[198.375px] top-[375.4px] w-[73.75px] h-[30.3px] rounded-full transition-colors hover:bg-neutral-300/10 text-blue-400 hover:text-blue-500'></Link>
+      <Link to='/' className='absolute left-[198.375px] top-[375.4px] w-[73.75px] h-[30.3px] rounded-full transition-colors hover:bg-neutral-300/10 text-blue-400 hover:text-blue-500'></Link>
       <Outlet />
     </>
   )
