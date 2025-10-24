0a1b19df489f8c513fff8ff5b47f945ba9604991
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-24 11:45:48 -0300

    feat: :sparkles: mejorar store del audio global

--- app/stores/useAudioStore.ts
+++ app/stores/useAudioStore.ts
@@ -1,14 +1,39 @@
 import { create } from 'zustand'
 
 type AudioStore = {
-  paused: boolean
+  audio: HTMLAudioElement | null
+  isPlaying: boolean
   src?: string
-  play?: () => void
-  pause?: () => void
+  setAudio: (newAudio: HTMLAudioElement) => void
+  play: (src?: string) => void
+  pause: () => void
+  setIsPlaying: (newState: boolean) => void
   setSource: (src: string) => void
 }
 
-export const useAudioStore = create<AudioStore>((set) => ({
-  paused: true,
+export const useAudioStore = create<AudioStore>((set, get) => ({
+  audio: null,
+  isPlaying: false,
+  setAudio: (newAudio) => set({ audio: newAudio }),
+  play (src) {
+    const { audio, setSource } = get()
+
+    if (src) {
+      setSource(src)
+    }
+
+    if (!audio) return
+
+    audio.play()
+      .then(() => set({ isPlaying: true }))
+      .catch((err) => {
+        console.error('error iniciando el video:', err)
+      })
+  },
+  pause () {
+    get().audio?.pause()
+    set({ isPlaying: false })
+  },
+  setIsPlaying: (newState) => set({ isPlaying: newState }),
   setSource: (src) => set({ src })
 }))
