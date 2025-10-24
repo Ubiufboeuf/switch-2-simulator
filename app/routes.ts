017156ddc3c9783d63fca9a92dfa2f07437c31fc
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-24 01:33:51 -0300

    feat: :sparkles: añadir página para crear mapas
    esos mapas son las estructuras por la que te puedes mover en la ui usando el teclado o mando

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,8 @@ import { type RouteConfig, index, layout, route } from '@react-router/dev/routes
 
 export default [
   layout('layouts/console.tsx', [
-  index('routes/startup.tsx'),
-  route('/home', 'routes/home.tsx')
+    index('routes/startup.tsx'),
+    route('/home', 'routes/home.tsx')
   ]),
+  route('/map-creator', 'routes/map_creator.tsx')
 ] satisfies RouteConfig
---
+++ app/routes/map_creator.tsx
@@ -0,0 +1,5 @@
+export default function MapCreator () {
+  return (
+    'editor'
+  )
+}
