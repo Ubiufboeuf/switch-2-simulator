4cd019e7c9a4180791dda027844a150524d4f68a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-24 01:35:22 -0300

    feat: comenzar estructura de /home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import { title } from '~/lib/utils'
 import type { Route } from './+types/home'
+import { useEffect, useRef } from 'react'
 
 export function meta ({}: Route.MetaArgs) {
   return [
@@ -9,11 +10,35 @@ export function meta ({}: Route.MetaArgs) {
 }
 
 export default function Home () {
+  const screenRef = useRef<HTMLElement>(null)
+
+  async function loadBoxesMap () {
+    // const map = new FocusMap()
+  }
+  
+  function loadBoxProfiles () {
+    
+  }
+  
+  useEffect(() => { (async () => {
+    await loadBoxesMap()
+    loadBoxProfiles()
+    // loadBoxTopInfo()
+    // loadBoxGames()
+    // loadBoxNavigation()
+    // loadBoxControls()
+    // loadBoxButtons()
+  })() }, [])
+  
   return (
-    <main id='screen' className='absolute z-1 w-[52.75vw] aspect-[16/10] mr-[0.5vw] mt-[2vh] flex items-center justify-center bg-black'>
-      <section className='h-full w-full flex items-center justify-center bg-[#1A1A1A]'>
-        
-      </section>
+    <main
+      ref={screenRef}
+      id='screen'
+      className='absolute z-1 w-[52.75vw] aspect-[16/10] mr-[0.5vw] mt-[2vh] flex items-center justify-center bg-[#1A1A1A] [&>*]:animate-fade-in [&>*]:animate-delay-300 [&>*]:animate-duration-200'
+    >
+      <div className='absolute'>
+        <img src='/switchui/images/home-semi-loaded.png' />
+      </div>
     </main>
   )
 }
