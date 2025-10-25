876be7acc109295ac1b55b93c27569dbd461f394
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-25 19:37:51 -0300

    feat: añadir funciones útiles

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -18,3 +18,25 @@ export function getPositionInCamera (coordAxis: 'left' | 'top' = 'left', positio
 
   return position
 }
+
+export function convertCSSUnitToNumber (unit: string | CSSStyleValue | undefined) {
+  if (!unit) return 0
+
+  const unitAsString = unit.toString()
+  let numberAsString = ''
+
+  for (const letter of unitAsString.split('')) {
+    if (Number(letter) || Number(letter) === 0) {
+      numberAsString = numberAsString.concat(letter)
+    }
+  }
+  
+  return Number(numberAsString) || 0
+}
+
+export function limitNumber (num: number = 0, min: number = -1, max: number = 1) {
+  let n = num
+  n = Math.min(max, num)
+  n = Math.max(min, n)
+  return n
+}
