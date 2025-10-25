e3eef72ed1c406a866a55037e1402f717f8d57a4
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-24 23:29:55 -0300

    feat: :lipstick: aplicar fondo a la sección de navegación
    se me olvidó hacerlo antes

--- app/compontents/home/SectionNavigation.tsx
+++ app/compontents/home/SectionNavigation.tsx
@@ -5,7 +5,7 @@ export function SectionNavigation () {
   const links = useNavigationStore((state) => state.homeLinks)
   
   return (
-    <section className='absolute top-[311.5px] left-1/2 -translate-x-1/2 w-[473.75px] h-fit flex items-center justify-end p-[8.5px] gap-[10px] rounded-full'>
+    <section className='absolute top-[311.5px] left-1/2 -translate-x-1/2 w-[473.75px] h-fit flex items-center justify-end p-[8.5px] gap-[10px] rounded-full bg-[#2C2C2C]'>
       { links.map(({ id, src }) => (
         <article key={id} className='first:mr-[6.75px] size-9'>
           { src && (
