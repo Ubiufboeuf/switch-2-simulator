f86959a0e353a5125b260a8f40140c0f7ec79194
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-24 23:55:42 -0300

    feat: mejorar identificadores y datos de las secciones y cajas visuales

--- app/compontents/home/SectionGames.tsx
+++ app/compontents/home/SectionGames.tsx
@@ -5,9 +5,16 @@ export function SectionGames () {
   const installedGames = useGamesStore((state) => state.installedGames)
 
   return (
-    <section className='section absolute top-[134.5px] left-0 w-full h-[145.5px] px-15 flex items-center gap-[7.5px]'>
+    <section
+      className='visual-section absolute top-[134.5px] left-0 w-full h-[145.5px] px-15 flex items-center gap-[7.5px]'
+      data-section-name='section-games'
+    >
       { installedGames.map(({ id, assets }) => (
-        <article key={`section-profiles-${id}`} className='box h-full aspect-square'>
+        <article
+          key={`box-games-${id}`}
+          className='visual-box h-full aspect-square'
+          data-box-id={id}
+        >
           <CoverArt src={assets.coverArt} />
         </article>
       )) }
--- app/compontents/home/SectionProfiles.tsx
+++ app/compontents/home/SectionProfiles.tsx
@@ -18,9 +18,16 @@ const profiles = [
 
 export function SectionProfiles () {
   return (
-    <section className='section absolute left-[27.5px] top-[47px] h-[35.75px] w-[120px] rounded-full flex gap-[6.5px]'>
+    <section
+      className='visual-section absolute left-[27.5px] top-[47px] h-[35.75px] w-[120px] rounded-full flex gap-[6.5px]'
+      data-section-name='section-profiles'
+    >
       { profiles.map(({ id, name }) => (
-        <article key={`section-profiles-${id}`} className='box h-full'>
+        <article
+          key={`box-profiles-${id}`}
+          className='visual-box h-full'
+          data-game-id={id}
+        >
           <Icon className='bg-black/50 h-full w-[35.75px] rounded-full overflow-hidden'>
             <Avatar name={name} className='text-[4px]' />
           </Icon>
