47ea5e1f53d78f0713892d72435a4f5531d14105
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-26 17:16:55 -0300

    refactor: :fire: eliminar creador de mapas

--- app/routes.ts
+++ app/routes.ts
@@ -4,6 +4,5 @@ export default [
   layout('layouts/ConsoleLayout.tsx', [
     index('routes/startup.tsx'),
     route('/home', 'routes/home.tsx')
-  ]),
-  route('/map-creator', 'routes/map_creator.tsx')
+  ])
 ] satisfies RouteConfig
