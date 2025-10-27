a91b6a5b8c0350fe843683a1631e656818701288
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-27 15:20:12 -0300

    feat: quitar la mayoría de mocks de la sección perfiles

--- app/compontents/home/SectionProfiles.tsx
+++ app/compontents/home/SectionProfiles.tsx
@@ -1,35 +1,35 @@
 import { Avatar } from '../Images/Avatar'
 import { Icon } from '../Images/Icon'
-
-const profiles = [
-  {
-    id: 'profile-1',
-    name: 'mario2'
-  },
-  {
-    id: 'profile-2',
-    name: 'luigi2'
-  },
-  {
-    id: 'profile-3',
-    name: 'donkeyKong2'
-  }
-]
+import { useEffect, useState } from 'react'
+import type { SectionItem } from '~/types/sectionTypes'
+import { useMapStore } from '~/stores/useMapStore'
 
 export function SectionProfiles () {
+  const map = useMapStore((state) => state.map)
+  const [profiles, setProfiles] = useState<SectionItem[]>([])
+
+  useEffect(() => {
+    if (!map) return
+
+    const section = map.items.find((s) => s.name === 'section-profiles')
+
+    if (!section) return
+    setProfiles(section.items)
+  }, [map])
+  
   return (
     <section
       className='visual-section absolute left-[27.5px] top-[47px] h-[35.75px] w-[120px] rounded-full flex gap-[6.5px]'
       data-section-name='section-profiles'
     >
-      { profiles.map(({ id, name }) => (
+      { profiles.map(({ id }) => (
         <article
           key={`box-profiles-${id}`}
           className='visual-box h-full'
           data-game-id={id}
         >
           <Icon className='bg-black/50 h-full w-[35.75px] rounded-full overflow-hidden'>
-            <Avatar name={name} className='text-[4px]' />
+            <Avatar name={'luigi2'} className='text-[4px]' />
           </Icon>
         </article>
       )) }
