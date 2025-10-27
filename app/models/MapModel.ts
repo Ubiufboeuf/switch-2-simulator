9ef028bf73123e1342523428535d1e654f825c3a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-27 15:56:06 -0300

    feat: mejorar tipos y modelo del mapa

--- app/models/MapModel.ts
+++ app/models/MapModel.ts
@@ -1,10 +1,21 @@
-import type { MapItem } from '~/types/mapTypes'
+import type { MapItem, MapProps } from '~/types/mapTypes'
+import type { Size } from '~/types/ui'
+import type { MapPreset } from '~/types/mapPresets'
+import type { Point } from '~/env'
 
-export class Map {
+export class Map implements MapPreset {
   id: string
+  name: string
+  path: string
+  initialCursorPosition: Point
+  virtualSize: Size
   items: MapItem[] = []
 
-  constructor (id?: string) {
+  constructor ({ id, initialCursorPosition, name, path, virtualSize }: MapProps) {
     this.id = id ?? crypto.randomUUID()
+    this.initialCursorPosition = initialCursorPosition
+    this.name = name
+    this.path = path
+    this.virtualSize = virtualSize
   }
 }
--- app/types/mapTypes.ts
+++ app/types/mapTypes.ts
@@ -1,3 +1,13 @@
+import type { Point } from '~/env'
 import type { Section } from '~/models/SectionModel'
+import type { Size } from './ui'
 
 export type MapItem = Section
+
+export type MapProps = {
+  id?: string
+  name: string
+  path: string
+  initialCursorPosition: Point
+  virtualSize: Size
+}
