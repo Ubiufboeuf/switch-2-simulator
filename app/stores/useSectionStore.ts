3836d12b9939e40798775708af11031fba5ba132
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-27 15:00:52 -0300

    refactor: reorganizar y renombrar funciones de la store de secciones

--- app/stores/useSectionStore.ts
+++ app/stores/useSectionStore.ts
@@ -4,18 +4,18 @@ import { Section } from '~/models/SectionModel'
 
 type SectionStore = {
   getSectionInMap: (id: string, map: Map) => Section | undefined
-  createSectionInMap: (map: Map) => Section
+  createSection: (name: string, map: Map) => Section
 }
 
-export const useSectionStore = create<SectionStore>((set) => ({
+export const useSectionStore = create<SectionStore>(() => ({
+  createSection (name, map) {
+    const section = new Section(name, map.id)
+    
+    return section
+  },
   getSectionInMap (id, map) {
     const { items } = map
     const section = items.find((i) => i.id === id)
-    return section
-  },
-  createSectionInMap (map) {
-    const section = new Section(map.id)
-    
     return section
   }
 }))
