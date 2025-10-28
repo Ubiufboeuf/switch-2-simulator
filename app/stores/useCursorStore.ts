eb85c08969798bba924a083099e1c15510b3d01b
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-27 23:57:49 -0300

    feat: mejorar store del cursor

--- app/stores/useCursorStore.ts
+++ app/stores/useCursorStore.ts
@@ -3,20 +3,25 @@ import type { Point } from '~/env'
 import { CursorModel } from '~/models/CursorModel'
 
 type CursorStore = {
-  readonly cursor: CursorModel
+  cursor: CursorModel
   boxId: string | null
   direction: Point
   virtualPosition: Point
+  initCursor: (initialPosition: Point) => void
   setBoxId: (newBoxId: string) => void
   setDirection: (newDirection: Point) => void
   setVirtualPosition: (newVirtualPosition: Point) => void
 }
 
-export const useCursorStore = create<CursorStore>((set) => ({
-  cursor: new CursorModel(),
-  boxId: 'game-0',
+export const useCursorStore = create<CursorStore>((set, get) => ({
+  cursor: new CursorModel({ x: 0, y: 1 }),
+  boxId: 'box-game-1',
   direction: { x: 0, y: 0 },
   virtualPosition: { x: 0, y: 0 },
+  initCursor: (initialPosition) => {
+    if (get().cursor) return
+    set({ cursor: new CursorModel(initialPosition) })
+  },
   setBoxId: (boxId) => set({ boxId }),
   setDirection : (direction) => set({ direction }),
   setVirtualPosition: (virtualPosition) => set({ virtualPosition })
