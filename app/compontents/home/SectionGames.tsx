10127d19943578abc397b2268130e4b00c15de2a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-29 19:16:34 -0300

    refactor(games): cambiar de dónde se sacan los juegos para home

--- app/compontents/home/SectionGames.tsx
+++ app/compontents/home/SectionGames.tsx
@@ -1,15 +1,14 @@
-import { useGamesStore } from '~/stores/useGamesStore'
 import { BoxGame } from '../boxes/BoxGame'
 
 export function SectionGames () {
-  const installedGames = useGamesStore((state) => state.installedGames)
+  const { principalGames } = useUserGames()
 
   return (
     <section
       className='visual-section absolute top-[134.5px] left-0 w-full h-[145.5px] px-15 flex items-center gap-[7.5px]'
       data-section-name='section-games'
     >
-      { installedGames.map((game) => (
+      { principalGames.map((game) => (
         <BoxGame key={`box-games-${game.id}`} game={game} />
       )) }
     </section>
