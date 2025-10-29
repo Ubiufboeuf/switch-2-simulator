d20c97ba1b7f31f2d858dd2ed7255e98d5c7a48c
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-29 19:12:13 -0300

    feat: mejorar store del mapa

--- app/stores/useMapStore.ts
+++ app/stores/useMapStore.ts
@@ -10,7 +10,9 @@ type MapStore = {
   initialCursorPosition: Point | null
   createMap: (preset: MapPreset) => Map
   addItem: (item: MapItem) => void
+  addSectionItem: (sectionId: string, item: SectionItem) => void
   getBoxByPosition: (position: Point) => SectionItem | undefined
+  getSectionById: (sectionId: string) => MapItem | undefined
 }
 
 export const useMapStore = create<MapStore>((set, get) => ({
@@ -36,11 +38,37 @@ export const useMapStore = create<MapStore>((set, get) => ({
       }
     }))
   },
+  addSectionItem (id, item) {
+    const { map } = get()
+    if (!map) return
+
+    const sectionIdx = map.items.findIndex((s) => s.id === id)
+    if (sectionIdx === -1) return
+
+    const section = map.items[sectionIdx]
+
+    set({ map:
+      { ...map, items:
+        [ ...map.items,
+          { ...section, items:
+            [ ...section.items, item ]
+          }
+        ]
+      }
+    })
+  },
   getBoxByPosition ({ x, y }) {
     const { map } = get()
     if (!map) return
 
     const box = map.items?.[y]?.items?.[x]
     return box
+  },
+  getSectionById (id) {
+    const { map } = get()
+    if (!map) return
+
+    const section = map.items.find((s) => s.id === id)
+    return section
   }
 }))
