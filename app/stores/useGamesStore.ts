e84850e815c97e0ccbf94169e871c8705748c101
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-29 19:14:55 -0300

    refactor(games): :fire: eliminar iteración para crear los juegos

--- app/stores/useGamesStore.ts
+++ app/stores/useGamesStore.ts
@@ -1,25 +1,5 @@
 import { create } from 'zustand'
-import { Game } from '~/models/BoxModel'
-
-const baseGames: Game[] = []
-
-for (let i = 0; i < 5; i++) {
-  const game = new Game({
-    id: `box-game-${i+1}`,
-    gameName: 'Mario Kart 8 Deluxe',
-    isDigital: false,
-    isInserted: false,
-    assets: {
-      coverArt: '/switchui/images/games-cover-art/mario_kart_8_deluxe-256.avif'
-    },
-    position: {
-      x: i,
-      y: 1
-    }
-  })
-
-  baseGames.push(game)
-}
+import type { Game } from '~/models/BoxModel'
 
 type GamesStore = {
   installedGames: Game[]
@@ -27,6 +7,6 @@ type GamesStore = {
 }
 
 export const useGamesStore = create<GamesStore>((set) => ({
-  installedGames: baseGames,
+  installedGames: [],
   setInstalledGames: (installedGames) => set({ installedGames })
 }))
