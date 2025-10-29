29276701a25b7a61e5871dd0d42ce5392a038cd2
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-29 14:02:22 -0300

    feat: mejorar store del cursor

--- app/stores/useCursorStore.ts
+++ app/stores/useCursorStore.ts
@@ -1,33 +1,64 @@
 import { create } from 'zustand'
 import type { Point } from '~/env'
 import { CursorModel } from '~/models/CursorModel'
-import type { Direction, DirectionAsPoint } from '~/types/ui'
+import type { Animation, Direction, DirectionAsPoint } from '~/types/ui'
 import { limitNumber } from '~/lib/utils'
 
 type CursorStore = {
   cursor: CursorModel | null
   boxId: string | null
+  boxElement: HTMLElement | null,
   direction: Point
   virtualPosition: Point
+  lastVirtualPosition: Point
+  cursorAnimation: Animation | null
+  setCursorAnimation: (animation: Animation) => void
   initCursor: (initialPosition: Point) => void
   setBoxId: (newBoxId: string) => void
+  setBoxElement: (newElement: HTMLElement) => void
   setDirection: (newDirection: Point) => void
   setVirtualPosition: (newVirtualPosition: Point) => void
+  addVirtualPosition: (direction: Point) => void
+  revertCursorMovement: () => void
   changeDirection: (action: 'press' | 'release', desiredDirection: Direction) => DirectionAsPoint
 }
 
 export const useCursorStore = create<CursorStore>((set, get) => ({
   cursor: null,
   boxId: 'box-game-1',
+  boxElement: null,
   direction: { x: 0, y: 0 },
-  virtualPosition: { x: 0, y: 0 },
+  virtualPosition: { x: 0, y: 1 },
+  lastVirtualPosition: { x: 0, y: 0 },
+  cursorAnimation: null,
+  setCursorAnimation: (cursorAnimation) => set({ cursorAnimation }),
   initCursor: (initialPosition) => {
     if (get().cursor) return
     set({ cursor: new CursorModel(initialPosition) })
   },
   setBoxId: (boxId) => set({ boxId }),
+  setBoxElement: (boxElement) => set({ boxElement }),
   setDirection : (direction) => set({ direction }),
-  setVirtualPosition: (virtualPosition) => set({ virtualPosition }),
+  setVirtualPosition (virtualPosition) {
+    set((state) => ({
+      virtualPosition,
+      lastVirtualPosition: state.virtualPosition
+    }))
+  },
+  addVirtualPosition (direction) {
+    set((state) => ({
+      virtualPosition: {
+        x: state.virtualPosition.x + direction.x,
+        y: state.virtualPosition.y + direction.y
+      },
+      lastVirtualPosition: state.virtualPosition
+    })
+  )},
+  revertCursorMovement () {
+    set((state) => ({
+      virtualPosition: state.lastVirtualPosition
+    }))
+  },
   changeDirection (action, desiredDirection) {
     const { cursor } = get()
     if (!cursor) {
