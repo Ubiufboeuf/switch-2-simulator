ac25addead72e8e62e58da86036d7a6df888d152
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-08 23:33:26 -0300

    feat: añadir etiquetas meta a la página de inicio

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,13 @@
+import { web_description, handleWebTitle } from '~/lib/utils'
+import type { Route } from './+types/home'
+
+export function meta ({}: Route.MetaArgs) {
+  return [
+    { title: handleWebTitle('Página de inicio') },
+    web_description
+  ]
+}
+
 export default function Home () {
   return (
     'home'
