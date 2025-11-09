a037cc81525002c7f2b7089bca783ae859717c04
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-08 22:29:39 -0300

    feat: :sparkles: añadir layout principal
    muestra una imagen de la consola (en este caso steamdeck) para mostrar como si fuese la real

---
+++ app/layouts/ConsoleLayout.tsx
@@ -0,0 +1,16 @@
+import { Link, Outlet } from 'react-router'
+
+export default function ConsoleLayout () {
+  return (
+    <div id='console-layout' className='flex items-center justify-center h-full min-h-fit clw:w-clw'>
+      <div className='w-341 min-w-341'>
+        <img src='/console.svg' className='w-full h-full' />
+      </div>
+      <Link
+        to='/'
+        className='absolute left-[198.375px] top-[375.4px] w-[73.75px] h-[30.3px] rounded-full transition-colors hover:bg-neutral-300/10'
+      />
+      <Outlet />
+    </div>
+  )
+}
--- app/routes.tsx
+++ app/routes.tsx
@@ -1,5 +1,7 @@
-import { index, type RouteConfig } from '@react-router/dev/routes'
+import { index, layout, type RouteConfig } from '@react-router/dev/routes'
 
 export default [
-  index('routes/index.tsx')
+  layout('layouts/ConsoleLayout.tsx', [
+    index('routes/index.tsx')
+  ])
 ] satisfies RouteConfig
