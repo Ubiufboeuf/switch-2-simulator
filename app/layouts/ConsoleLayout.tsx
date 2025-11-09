240c79eeec17e0362b67e98af7ad5263981576eb
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-08 23:14:07 -0300

    fix: :lipstick: arreglar tamaño del ConsoleLayout
    esto arregla la posición de los elementos hijos. Va un poco de la mano con el arreglo anterior sobre lo mismo

--- app/layouts/ConsoleLayout.tsx
+++ app/layouts/ConsoleLayout.tsx
@@ -5,7 +5,7 @@ export default function ConsoleLayout () {
   return (
     <div
       id='console-layout'
-      className='relative flex items-center justify-center h-full min-h-clh w-clw min-w-clw'
+      className='relative flex items-center justify-center h-fit min-h-clh w-fit min-w-clw'
     >
       <Link
         to='/'
