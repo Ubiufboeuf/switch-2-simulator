bd4b841734cb1eb3fcfd8e68fa30384d46039938
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-08 22:41:15 -0300

    feat: :lipstick: mejorar visualmente el componente Camera

--- app/components/Camera.tsx
+++ app/components/Camera.tsx
@@ -4,7 +4,7 @@ export function Camera ({ children }: { children: ReactNode }) {
   return (
     <main
       id='camera'
-      className='absolute'
+      className='absolute z-1 mr-[6px] mt-[12px] flex items-center justify-center w-181 h-[450.5px] bg-[#1A1A1A]'
     >
       {children}
     </main>
