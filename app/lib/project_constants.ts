bbd7b9c60c5ffff8e69e2c27b702618f3ed7465e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-08 23:29:02 -0300

    feat: añadir constantes del proyecto

---
+++ app/lib/project_constants.ts
@@ -0,0 +1,2 @@
+export const BASE_TITLE = 'Nintendo Switch 2'
+export const WEB_DESCRIPTION = 'Simulador de Nintendo Switch 2, hecho con react-router v7, para disfrutar del encanto de la consola, al menos visualmente.'
