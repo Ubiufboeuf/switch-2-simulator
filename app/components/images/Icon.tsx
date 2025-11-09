bdaad27859d428de4621176d25eadec7e0d3560f
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-09 00:21:36 -0300

    feat: crear componente Icon
    wrapper para imágenes con algunos preestilos para añadir imágenes más comodamente

---
+++ app/components/images/Icon.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from 'react'
+
+export function Icon ({ className = '', children }: { className?: string, children: ReactNode }) {
+  return (
+    <div className={`${className} not-[[class*=h-]]:h-10 not-[[class*=w-]]:w-10 flex items-center justify-center`}>
+      {children}
+    </div>
+  )
+}
