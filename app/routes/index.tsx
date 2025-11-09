fa3571506c659bb16b13575c2cbb0bea9f71bf7b
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-08 23:30:07 -0300

    feat: actualizar página inicial

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,72 @@
+import { useEffect, useRef, type SyntheticEvent } from 'react'
+import { useNavigate } from 'react-router'
+import { web_description, web_title } from '~/lib/utils'
+import { useSwitchStore } from '~/stores/useSwitchStore'
+import type { Route } from './+types/index'
+
+export function meta ({}: Route.MetaArgs) {
+  return [
+    web_title,
+    web_description
+  ]
+}
+
 export default function Index () {
+  const navigate = useNavigate()
+  
+  const isConsoleOn = useSwitchStore((state) => state.isConsoleOn)
+  const setIsConsoleOn = useSwitchStore((state) => state.setIsConsoleOn)
+  
+  const startupVideoRef = useRef<HTMLVideoElement | null>(null)
+
+  function handleToggleConsoleState () {
+    setIsConsoleOn(true)
+  }
+
+  function handleStartupVideoTimeUpdate (event: SyntheticEvent<HTMLVideoElement>) {
+    const video = event.currentTarget
+    const { currentTime } = video
+    
+    // NOTA: Esto es para acelerar tiempos por ahora
+    // if (currentTime < 5) video.currentTime = 5
+  }
+
+  function handleStartupVideoEnded () {
+    navigate('/home')
+  }
+
+  useEffect(() => {
+    if (!isConsoleOn) return
+    const startupVideo = startupVideoRef.current
+
+    if (!startupVideo) return
+
+    // Simular demora de encendido
+    setTimeout(() => {
+      startupVideo.play()
+    }, 2000)
+  }, [isConsoleOn])
+  
   return (
-    'index'
+    <>
+      <button
+        className={`${isConsoleOn ? 'consoleOn' : ''}
+          [.consoleOn]:opacity-0 [.consoleOn]:pointer-events-none [.consoleOn]:select-none
+          absolute h-full w-full cursor-pointer outline-0 z-2 transition-all
+        `}
+        onClick={handleToggleConsoleState}
+      >
+        Toca la pantalla para encender la consola
+      </button>
+      <section className='h-full w-full flex items-center justify-center bg-[#1A1A1A]'>
+        <video
+          ref={startupVideoRef}
+          src='/switchui/movies/startup.mp4'
+          className='object-cover w-full h-full'
+          onTimeUpdate={handleStartupVideoTimeUpdate}
+          onEnded={handleStartupVideoEnded}
+        />
+      </section>
+    </>
   )
 }
