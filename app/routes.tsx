55396fa78a6180bea0a27f43d6a9d1671702fc98
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-08 23:27:00 -0300

    feat: agregar ruta para la pantalla de inicio

--- app/routes.tsx
+++ app/routes.tsx
@@ -1,7 +1,8 @@
-import { index, layout, type RouteConfig } from '@react-router/dev/routes'
+import { index, layout, route, type RouteConfig } from '@react-router/dev/routes'
 
 export default [
   layout('layouts/ConsoleLayout.tsx', [
-    index('routes/index.tsx')
+    index('routes/index.tsx'),
+    route('/home', 'routes/home.tsx')
   ])
 ] satisfies RouteConfig
