b085f09ab7fa7002d2e0875ca55954cce83b528f
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-13 20:29:27 -0300

    feat: usar atributo hidden en Camera
    es por ahora, para que simplemente no aparezca

--- app/components/Camera.tsx
+++ app/components/Camera.tsx
@@ -7,7 +7,7 @@ export function Camera ({ children }: { children: ReactNode }) {
       id='camera'
       className='absolute z-1 mr-[6px] mt-[12px] flex items-center justify-center w-181 h-[450.5px] bg-[#1A1A1A]'
     >
-      <Cursor />
+      <Cursor hidden />
       {children}
     </main>
   )
