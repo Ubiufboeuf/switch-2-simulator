160c8422bdf1cec92daaae7b83387b7c4c03ae1f
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 13:16:10 -0300

    feat: actualizar servicio del mapa

--- app/services/mapService.ts
+++ app/services/mapService.ts
@@ -1,22 +1,37 @@
+import { ENDPOINTS } from '~/lib/endpoints'
+import { isValidMap } from '~/lib/validations'
 import { useMapStore } from '~/stores/useMapStore'
 import type { Box } from '~/types/boxTypes'
 import type { Map } from '~/types/mapTypes'
+import { createBox } from './boxService'
 
-export function useLoadMap (pathname: string) {
-  const { map, setMap } = useMapStore.getState()
-  if (map) return
+export async function useLoadMap (pathname: string) {
+  const state = useMapStore.getState()
+  if (state.map) return
 
-  const newMap = loadMap(pathname)
-  if (!newMap) return
-  
-  setMap(newMap)
+  const { setMap } = state
+
+  let map: Map | undefined
+  try {
+    map = await fetchMap(pathname)
+  } catch (err) {
+    console.error('error cargando el mapa', err)
+  }
+
+  if (!map) return
+  setMap(map)
+
+  for (const item of map.items) {
+    const box = createBox({ id: item.id })
+    addBoxToMap(box)
+  }
 }
 
-export function loadMap (pathname: string): Map {
-  console.log('loadMap', pathname)
-  const map: Map = {
-    id: 'map-1'
-  }
+export async function fetchMap (pathname: string): Promise<Map | undefined> {
+  const res = await fetch(ENDPOINTS.MAPS['home-map'])
+  const map = await res.json()
+
+  if (!isValidMap(map)) return
 
   return map
 }
