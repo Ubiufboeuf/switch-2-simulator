b1d98ad8923d361cedae4031ce9a0934fd4fe9f2
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 19:49:37 -0300

    feat: actualizar función para crear cajas

--- app/services/boxService.ts
+++ app/services/boxService.ts
@@ -2,7 +2,9 @@ import type { Box, CreateBoxProps } from '~/types/boxTypes'
 
 export function createBox (props?: CreateBoxProps): Box {
   const box: Box = {
-    id: props?.id ?? crypto.randomUUID()
+    id: props?.id ?? crypto.randomUUID(),
+    selected: props?.selected,
+    topology: props?.topology
   }
   
   return box
