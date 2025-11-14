dc4ac40727f2ec1d9050d334a0954f6a07353bbb
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 19:53:22 -0300

    feat: mejorar store del mapa
    añadir items y hacer que addItem afecte tanto al mapa como a los items

--- app/stores/useMapStore.ts
+++ app/stores/useMapStore.ts
@@ -3,21 +3,26 @@ import type { Map, MapItem } from '~/types/mapTypes'
 
 type MapStore = {
   map: Map | null
+  items: MapItem[] | null
   setMap: (map: Map) => void
   addItem: (item: MapItem) => void
 }
 
 export const useMapStore = create<MapStore>((set) => ({
   map: null,
+  items: null,
   setMap: (map) => set({ map }),
   addItem (item) {
     set(({ map }) => {
       if (!map) return {}
+      const newItems = [...(map.items)]
+      if (!newItems.some((i) => i.id === item.id)) newItems.push(item)
       return {
         map: {
           ...map,
-          items: [...map.items, item]
-        }
+          items: [...newItems]
+        },
+        items: [...newItems]
       }
     })
   }
