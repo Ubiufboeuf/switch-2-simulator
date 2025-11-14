50e88db42ed52d4008d85a93dc397189ddae60ea
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 20:46:29 -0300

    feat(cursor): :lipstick: añadir borde animado al cursor

--- app/components/Cursor.tsx
+++ app/components/Cursor.tsx
@@ -4,7 +4,7 @@ const borderSpacing = 2
 const borderWidth = 2
 
 export function Cursor ({ hidden }: { hidden?: boolean }) {
-  const { cursorStyles } = useCursor({ borderSpacing, borderWidth })
+  const { cursorStyles, borderStyles } = useCursor({ borderSpacing, borderWidth })
   
   return (
     <article
@@ -12,6 +12,10 @@ export function Cursor ({ hidden }: { hidden?: boolean }) {
       className='absolute z-2 flex items-center justify-center'
       style={cursorStyles}
       hidden={hidden}
-    />
+    >
+      <div id='cursor-border' className='relative size-full overflow-visible' style={borderStyles}>
+        <div id='cursor-border-colors' className='absolute -left-1/2 -top-1/2 h-[200%] w-[200%] bg-blue-300/20' />
+      </div>
+    </article>
   )
 }
