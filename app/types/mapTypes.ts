7dd0189cb774bf852eddd2687289fbe679109e05
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 13:06:12 -0300

    feat: :label: actualizar tipos del mapa y añadir una caja seleccionada por defecto

--- app/types/mapTypes.ts
+++ app/types/mapTypes.ts
@@ -1,3 +1,12 @@
+import type { Direction } from '~/env'
+
 export interface Map {
   id: string
+  items: MapItem[]
+}
+
+export interface MapItem {
+  id: string
+  selected?: boolean
+  topology?: Record<Direction, string>
 }
