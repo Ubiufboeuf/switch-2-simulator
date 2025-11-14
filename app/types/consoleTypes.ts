1266b7ce0b70d0fe806789bf8411c7c4dcccf69a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 20:53:58 -0300

    feat: :label: actualizar tipos del cursor y de la consola

--- app/types/consoleTypes.ts
+++ app/types/consoleTypes.ts
@@ -1,1 +1,7 @@
 export type ConsoleState = 'on' | 'off'
+
+export type KeyboardKey = ' '
+  | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0'
+  | 'q' | 'w' | 'e' | 'r' | 't' | 'y' | 'u' | 'i' | 'o' | 'p'
+  | 'a' | 's' | 'd' | 'f' | 'g' | 'h' | 'j' | 'k' | 'l' | 'ñ'
+  | 'z' | 'x' | 'c' | 'v' | 'b' | 'n' | 'm' | ',' | '.' | '-'
--- app/types/cursorTypes.ts
+++ app/types/cursorTypes.ts
@@ -1,3 +1,6 @@
+import type { Direction, DirectionAsPoint } from '~/env'
+import type { KeyboardKey } from './consoleTypes'
+
 export interface CursorHookProps {
   borderSpacing: number
   borderWidth: number
@@ -5,4 +8,12 @@ export interface CursorHookProps {
 
 export interface Cursor {
   id: string
+  controller: {
+    directions: Record<Direction, boolean>,
+    directionAsPoint: DirectionAsPoint,
+    keyboard: {
+      key: KeyboardKey
+      direction: Direction
+    }[]
+  }
 }
