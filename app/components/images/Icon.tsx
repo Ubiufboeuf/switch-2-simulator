8eb456d990b810465d618cf679c85c0097d07ca3
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 00:01:29 -0300

    feat: :lipstick: actualizar componente Icon

--- app/components/images/Icon.tsx
+++ app/components/images/Icon.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react'
 
 export function Icon ({ className = '', children }: { className?: string, children: ReactNode }) {
   return (
-    <div className={`${className} not-[[class*=h-]]:h-10 not-[[class*=w-]]:w-10 flex items-center justify-center`}>
+    <div className={`${className} not-[[class*=h-]]:h-10 not-[[class*=w-]]:w-10 flex items-center justify-center overflow-hidden`}>
       {children}
     </div>
   )
