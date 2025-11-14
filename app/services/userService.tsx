424d4a25bd54ebf43fb1cf6962098c9eefb65f7b
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 01:36:37 -0300

    feat: :sparkles: crear servicio de usuario

---
+++ app/services/userService.tsx
@@ -0,0 +1,39 @@
+import { ENDPOINTS } from '~/lib/endpoints'
+import { useUserStore } from '~/stores/useUserStore'
+import type { User } from '~/types/userTypes'
+
+const USER_ID = 'user-1'
+
+export async function useLoadUser () {
+  let user = null
+  try {
+    user = await fetchUser(USER_ID)
+  } catch (err) {
+    console.error('error consiguiendo los datos del usuario', err)
+  }
+
+  if (!user) return
+
+  const { 
+    setUser, 
+    setUserId, 
+    setName, 
+    setUserName, 
+    setBirthDate, 
+    setAvatar 
+  } = useUserStore.getState()
+
+  setUser(user)
+  setUserId(user.id)
+  setName(user.profiles[0].name)
+  setUserName(user.profiles[0].userName)
+  setBirthDate(user.profiles[0].birthDate)
+  setAvatar(user.profiles[0].avatar)
+}
+
+export async function fetchUser (id: string): Promise<User | undefined> {
+  console.log(ENDPOINTS.USER)
+  const res = await fetch(ENDPOINTS.USER)
+  const user = await res.json()
+  return user
+}
