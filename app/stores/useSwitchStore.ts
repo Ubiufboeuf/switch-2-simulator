6596e60b2deda7f0d328b5478e1b799aa9cbf94d
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 00:19:49 -0300

    feat: actualizar store de la consola

--- app/stores/useSwitchStore.ts
+++ app/stores/useSwitchStore.ts
@@ -4,6 +4,7 @@ import type { ConsoleState } from '~/types/consoleTypes'
 type SwitchStore = {
   isConsoleOn: boolean
   consoleState: ConsoleState
+  userId: string
   setIsConsoleOn: (newState: boolean) => void
   setConsoleState: (newState: ConsoleState) => void
 }
@@ -11,6 +12,7 @@ type SwitchStore = {
 export const useSwitchStore = create<SwitchStore>((set) => ({
   isConsoleOn: false,
   consoleState: 'off',
+  userId: 'user-1',
   setIsConsoleOn: (newState) => set({ isConsoleOn: newState }),
   setConsoleState: (newState) => set({ consoleState: newState })
 }))
