b0b292378b5b27fbf71849931ccee89326697ba0
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 01:56:35 -0300

    feat(home): :sparkles: añadir sección de perfiles

---
+++ app/components/home/SectionProfiles.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react'
+import { Icon } from '../images/Icon'
+import { useMapStore } from '~/stores/useMapStore'
+import { Avatar } from '../images/Avatar'
+import { useUserStore } from '~/stores/useUserStore'
+
+export function SectionProfiles () {
+  const map = useMapStore((state) => state.map)
+  const user = useUserStore((state) => state.user)
+
+  useEffect(() => {
+    if (!map) return
+
+    // const section = map.items.find((s) => s.name === 'section-profiles')
+
+    // if (!section) return
+    // setProfiles(section.items)
+  }, [map])
+  
+  return (
+    <section
+      className='visual-section absolute left-[27.5px] top-[47px] h-[35.75px] w-[120px] rounded-full flex gap-[6.5px]'
+      data-section-name='section-profiles'
+    >
+      { user?.profiles.map(({ avatar }) => (
+        <article
+          key={`box-profiles-${avatar}`}
+          className='visual-box h-full'
+          data-profile-name={avatar}
+        >
+          <Icon className='bg-black/50 h-full w-[35.75px] rounded-full'>
+            <Avatar name={avatar} className='text-[4px]' />
+          </Icon>
+        </article>
+      )) }
+    </section>
+  )
+}
