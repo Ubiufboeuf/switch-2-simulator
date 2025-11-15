8bc931739dbb753fab4b23c46d5580418fd4cd3e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-14 22:03:19 -0300

    feat(cursor): :sparkles: actualizar store del cursor y crear cursor

--- app/stores/useCursorStore.ts
+++ app/stores/useCursorStore.ts
@@ -1,12 +1,28 @@
 import { create } from 'zustand'
 import type { Cursor } from '~/types/cursorTypes'
 
+const cursor: Cursor = {
+  controller: {
+    directions: {
+      up: false,
+      left: false,
+      down: false,
+      right: false
+    },
+    directionAsPoint: { x: 0, y: 0 },
+    keyboard: [
+      { key: 'w', direction: 'up' },
+      { key: 'a', direction: 'left' },
+      { key: 's', direction: 'down' },
+      { key: 'd', direction: 'right' }
+    ]
+  }  
+}
+
 type CursorStore = {
-  cursor: Cursor | null
-  setCursor: (cursor: Cursor) => void
+  cursor: Cursor
 }
 
 export const useCursorStore = create<CursorStore>((set) => ({
-  cursor: null,
-  setCursor: (cursor) => set({ cursor })
+  cursor
 }))
