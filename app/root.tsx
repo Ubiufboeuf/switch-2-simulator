e49f6cc36196f1f386d6ecba4e4c492c03d75eaa
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-19 01:42:51 -0300

    refactor: :truck: mover assets a switchui/

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ export function Layout ({ children }: { children: ReactNode }) {
       <head>
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='shortcut icon' href='/switchui/favicon.ico' type='image/x-icon' />
         <Meta />
         <Links />
       </head>
