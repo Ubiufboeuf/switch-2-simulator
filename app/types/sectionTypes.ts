8372eb84720c85dc28f383407e0e2559cdeb07fd
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-19 23:44:29 -0300

    feat: 🏷 actualizar tipos para coincidir con el esquema

--- app/types/sectionTypes.ts
+++ app/types/sectionTypes.ts
@@ -1,7 +1,10 @@
+import type { Direction } from '~/env'
 import type { Box } from './boxTypes'
 
 export interface Section {
   id: string
   type: 'section'
+  boxToReturn: string
+  topology?: Record<Direction, string>
   items: Box[]
 }
