ce4142b5f3244a3414a80ca74090187478d279a7
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-21 18:24:34 -0300

    style: :art: cambiar return de la lista de juegos en home

--- app/components/home/Games.tsx
+++ app/components/home/Games.tsx
@@ -17,23 +17,21 @@ export function Games () {
         {currentGame.title}
       </h1>
       <div className='flex items-center gap-[6.5px] h-[146px] mt-[27px] pl-[58.5px]'>
-        { user?.games.principalList.map(({ game, assets }, idx) => {
-          return (
-            <article
-              key={`box-game-${idx}`}
-              className='visual-box h-full rounded-[10px] aspect-square'
-              data-game-name={game}
-              data-box-id={`box-2-${idx + 1}`}
-            >
-              <div className='h-full w-full rounded-[inherit] overflow-hidden'>
-                <img
-                  src={assets.coverArt}
-                  className='h-full w-full object-cover'
-                />
-              </div>
-            </article>
-          )
-        }) }
+        { user?.games.principalList.map(({ game, assets }, idx) => (
+          <article
+            key={`box-game-${idx}`}
+            className='visual-box h-full rounded-[10px] aspect-square'
+            data-game-name={game}
+            data-box-id={`box-2-${idx + 1}`}
+          >
+            <div className='h-full w-full rounded-[inherit] overflow-hidden'>
+              <img
+                src={assets.coverArt}
+                className='h-full w-full object-cover'
+              />
+            </div>
+          </article>
+        )) }
       </div>
     </section>
   )
